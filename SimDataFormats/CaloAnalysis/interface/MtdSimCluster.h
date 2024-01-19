f0f88514b674fb14e78031054d1729e3bcb82def
Martina <martina.malberti@cern.ch>
2024-01-20 00:16:31 +0100

    updated newForm

--- SimDataFormats/CaloAnalysis/interface/MtdSimCluster.h
+++ SimDataFormats/CaloAnalysis/interface/MtdSimCluster.h
@@ -98,15 +98,21 @@ public:
   }
 
   // --  for backward compatibility
-  //std::vector<uint64_t> newForm(){
-  void newForm(){ 
+  void newForm(){
     static const uint32_t kMTDsubdOffset = 23;
     static const uint32_t kMTDsubdMask = 0x3;
+    
+    static constexpr uint32_t kBTLmoduleOffset = 10;
+    static constexpr uint32_t kBTLmoduleMask = 0x3F;
+    static constexpr uint32_t kBTLmodTypeOffset = 8;
+    static constexpr uint32_t kBTLmodTypeMask = 0x3;
+    static constexpr uint32_t kModulePerTypeBarPhiFlat = 48 / 3;
+
     static constexpr uint32_t kBTLRUOffset = 6;
     static constexpr uint32_t kBTLRUMask = 0x3;
     static constexpr uint32_t kBTLCrystalMask = 0x3F;
     static constexpr uint32_t kCrystalsPerModuleV2 = 16;
-    
+      
     for (size_t i = 0; i < mtdHits_.size(); ++i) {
       
       // - Get detId, row, column from old format
@@ -115,19 +121,23 @@ public:
       uint8_t col = static_cast<uint8_t>(mtdHits_[i]); 
             
       uint32_t newid = 0;
+
       // - Change bit content only for BTL ( MTD subdetector BTL/ETL : bit 24-23) 
       if ( ((rawid >> kMTDsubdOffset) & kMTDsubdMask ) == 1 ){ 
-	
-	// - check bit 6-7: if = 0 --> old barphiflat geometry, if !=0 V2, V3 geometry
+	// - check bit 6-7: if = 0 --> old barphiflat BTL layout, if !=0 V2, V3 BTL layout
 	if ( ((rawid >> kBTLRUOffset) & kBTLRUMask ) == 0){
-	  //newid = 0;
+	  // - change bit fields for module (15-10), crystal type (9-8), crystal num (7-0) as done in BTLDetId.cc for geom V1
+	  uint32_t module = (rawid >> kBTLmoduleOffset) & kBTLmoduleMask;
+	  uint32_t modtype = (rawid >> kBTLmodTypeOffset) & kBTLmodTypeMask;
+	  uint32_t newmodule = module + kModulePerTypeBarPhiFlat * (modtype - 1);
+	  newid = ((rawid >> kBTLmoduleOffset) & ~kBTLmoduleMask) | newmodule;
+	  newid = newid << kBTLmoduleOffset;
 	}
 	else {
+	  // - change bit field for crystal number to 17-1 for geom V2, V3
 	  newid = (~kBTLCrystalMask & rawid) | kCrystalsPerModuleV2;
 	}
 	
-	std::cout << "old Id = " <<  rawid << "  new Id = " << newid << std::endl;
-		
 	// - Create the unique id in the new format
 	uint64_t uniqueIdNew = static_cast<uint64_t>(newid) << 32;
 	uniqueIdNew |= row << 16;
@@ -136,7 +146,6 @@ public:
 	mtdHits_[i] = uniqueIdNew;
       }	
     }
-    //return mtdHits_;
   }
   
     
