a6b96de6b1d772328f6342465fe6670cbed1d9cc
Ruben Lopez Ruiz <24.lopezr@gmail.com>
2026-06-10 11:53:09 +0200

    Drop energy and position in ETL RecHit format + minor tweaks in ETL digi

--- DataFormats/FTLRecHitSoA/src/ETLRecHitSoA.cc
+++ DataFormats/FTLRecHitSoA/src/ETLRecHitSoA.cc
@@ -10,9 +10,7 @@ namespace etlrechit {
     out << "ETL rechit SoA: "
         << " detID: " << etlrh.detId().rawId() << ", row: " << static_cast<int>(etlrh.row())
         << ", column: " << static_cast<int>(etlrh.column()) << ", toa: " << etlrh.toa() << ", tot: " << etlrh.tot()
-        << ", energy: " << etlrh.energy() << ", position:	" << etlrh.position()
-        << ", toa error : " << etlrh.toa_error() << ", position error:	" << etlrh.position_error()
-        << ", flags: " << std::bitset<8>(etlrh.flags());
+        << ", toa error : " << etlrh.toa_error() << ", flags: " << std::bitset<8>(etlrh.flags());
     return out;
   }
 
