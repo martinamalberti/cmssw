68f0f40937aff80479fa0d75b3fe71851ceb17c4
Martina <martina.malberti@cern.ch>
2026-09-23 21:18:51 +0200

    bug fix the the tofhir partner channel array

--- EventFilter/MTDRawToDigi/interface/BTLElectronicsSpecs.h
+++ EventFilter/MTDRawToDigi/interface/BTLElectronicsSpecs.h
@@ -47,12 +47,10 @@ namespace BTLElectronicsSpecs {
   // Table of TOFHIR channel partners.
   // The array index is the TOFHIR channelId.
   // The value is the channelId of its partner.
-  // !!!!!!!!!! DA RICONTROLLARE !!!!
+  // !!!!!!!!!! DA RICONTROLLARE  !!!!
   static constexpr std::array<int8_t, 32> kTofhirChannelPartner = {
-   18, 16, 20, 19, 17, 22, 23, 21,
-    28, 26, 30, 27, 31, 29, 24, 25,
-     1,  4,  0,  3,  2,  7,  5,  6,
-    14, 15,  9, 11,  8, 13, 10, 12
+      24, 25, 31, 30, 29, 26, 28, 27, 23, 22, 19, 21, 20, 17, 18,
+      16, 15, 13, 14, 10, 12, 11, 9, 8, 0, 1, 5, 7, 6, 4, 3, 2
   };
   
 
